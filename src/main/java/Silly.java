415a31fa0808d6a6ba16d06044aa2487be592074
SanzSS <102633707+SanzSS@users.noreply.github.com>
2022-09-11 00:52:18 -0400

    Update Silly.java

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -206,7 +206,7 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
-        return this.name - other.toString();
+        return this.name.length() - other.toString().length();
     }
 
     /*
